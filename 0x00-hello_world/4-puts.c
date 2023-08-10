e4b322c4f4499048f6529a93721f16b05e2a74fb
Eioys <sharondoradora@gmail.com>
2023-08-10 08:24:41 -0700

    C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print the exact text followed by a new line using puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
