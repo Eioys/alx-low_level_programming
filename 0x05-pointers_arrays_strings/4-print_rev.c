3c84cba815cd1433f917a46d4f6c8125fab3f2be
Eioys <sharondoradora@gmail.com>
2023-08-21 10:23:09 -0700

    writing a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: parameter to be reversed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	int k  = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		k++;
+	for (i = k - 1; i >= 0; i--)
+		_putchar(s[i]);
+}
