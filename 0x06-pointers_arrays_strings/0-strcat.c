879b6716932ef71ac0e41b2975c484ec0e9a4495
Eioys <sharondoradora@gmail.com>
2023-08-23 06:24:59 -0700

    joinning two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: end
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+	srclen++;
+	for (i = 0 ; i <= srclen; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
