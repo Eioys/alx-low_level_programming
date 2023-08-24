75cf7ce1223dedca53fa73f2718ff857e9eef00a
Eioys <sharondoradora@gmail.com>
2023-08-24 12:31:24 -0700

    capital letters all

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,38 +2,39 @@
 
 /**
  * cap_string - function that capitalizes all words of a string
- * @s: string to capitalize
+ * @str: string to capitalize
  * Return: a pointer to string changed
  */
 
 char *cap_string(char *str)
 {
+
+	int index = 0;
+
+	while (str[index])
 	{
-		int index = 0;
-
-		while (str[index])
-		{
-			while (!(str[index] >= 'a' && str[index] <= 'z'))
-				index++;
-
-			if (str[index - 1] == ' ' ||
-					(str[index - 1]) == '\n' ||
-					(str[index - 1]) == '\t' ||
-					(str[index - 1]) == ',' ||
-					(str[index - 1]) == ';' ||
-					(str[index - 1]) == '.' ||
-					(str[index - 1]) == '!' ||
-					(str[index - 1]) == '?' ||
-					(str[index - 1]) == '"' ||
-					(str[index - 1]) == '(' ||
-					(str[index - 1]) == ')' ||
-					(str[index - 1]) == '{' ||
-					(str[index - 1]) == '}' ||
-					index == 0)
-				str[index] -= 32;
-
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		}
-		return (str);
+
+		if (str[index - 1] == ' ' ||
+				(str[index - 1]) == '\t' ||
+				(str[index - 1]) == '\n' ||
+				(str[index - 1]) == ',' ||
+				(str[index - 1]) == ';' ||
+				(str[index - 1]) == '.' ||
+				(str[index - 1]) == '!' ||
+				(str[index - 1]) == '?' ||
+				(str[index - 1]) == '"' ||
+				(str[index - 1]) == '(' ||
+				(str[index - 1]) == ')' ||
+				(str[index - 1]) == '{' ||
+				(str[index - 1]) == '}' ||
+				index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
+	return (str);
 }
+
+
