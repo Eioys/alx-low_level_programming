53b500c7dfb1a21bd2a1eab85f0f6f2f0f83eaf0
Eioys <sharondoradora@gmail.com>
2023-09-04 23:28:26 -0700

    copy string and return to pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - ceates space and enters the copied string in space
+ * @str: strig to be copied
+ * Return: NULL or pointer
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *copy;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	copy = malloc(sizeof(char) * count++);
+
+	if (copy == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
+	
