086fee2a38b339d138bea23e155bcd40c5a032e2
Eioys <sharondoradora@gmail.com>
2023-09-05 00:40:34 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
 
-	copy = malloc(sizeof(char) * count++);
+	copy = malloc(sizeof(char) * count + i);
 
 	if (copy == NULL)
 		return (NULL);
