a591cdf0fe1b2040a6779dd0315a68a6224e4c6d
Eioys <sharondoradora@gmail.com>
2023-09-11 07:53:11 -0700

    0X0d tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints name of filewas compiled followed by new line
+ * Return: 0
+ */
+
+int main (void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
