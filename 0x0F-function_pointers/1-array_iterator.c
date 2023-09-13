6933c8cde8941c2b0a34cce623be3fc8ffa7773c
Eioys <sharondoradora@gmail.com>
2023-09-13 11:16:15 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator -  executes fxn given as parameter on array[i]
+ * @size: size of array
+ * @array: actual array
+ * @action: function to perform on each element of the array
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+        unsigned int i;
+
+        if (array == NULL || action == NULL)
+                return;
+        for (i = 0; i < size; i++)
+        {
+                action(*array);
+                array++;
+        }
+
+}
