6db5788f028e209270cf7c1617ac701bed26636b
Eioys <sharondoradora@gmail.com>
2023-09-14 09:20:33 -0700

    prints any parameter

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print any argument to the function
+ * @format: format specifier
+ * Return: any argument given based on format specifier
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i, checker;
+	char *str;
+	va_list space;
+
+	va_start(space, format);
+
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(space, int));
+				checker = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(space, double));
+				checker = 0;
+				break;
+			case 'c':
+				printf("%c", va_arg(space, int));
+				checker = 0;
+				break;
+			case 's':
+				str = va_arg(space, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				checker = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && checker == 0)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(space);
+}
+
