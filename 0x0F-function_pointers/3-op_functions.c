6e732fb09608492c69d39d01982976e854b0c8e3
Eioys <sharondoradora@gmail.com>
2023-09-14 06:15:08 -0700

    arithematic operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns sum of two int
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - minus a and b
+ * @a: first int
+ * @b: second int
+ * Return: a- b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply a and b
+ * @a: first int
+ * @b: second int
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide a and b
+ * @a: first int
+ * @b: second int
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of a and b
+ * @a: first int
+ * @b: second int
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
